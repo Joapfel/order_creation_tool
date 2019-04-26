6346a932c945038d1b05226e95f373316e2b8a70
PhilippTeichert <34233007+PhilippTeichert@users.noreply.github.com>
2019-04-26 22:33:00 +0200

    Update User.java

--- src/de/uni_stuttgart/est/project/main/java/dao/User.java
+++ src/de/uni_stuttgart/est/project/main/java/dao/User.java
@@ -1,20 +1,47 @@
 package dao;
 
-public class User {
+import java.io.*;
+
+	/**
+	 * Class to store User-Data.
+	 * 
+	 * @
+	 * @author Philipp
+	 *
+	 */
+public class User implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4456805580456631474L;
 	
 	private String username;
 	private String password;
 	
+	/**
+	 * Constructor for Users.
+	 * 
+	 * @param username
+	 * @param password
+	 */
 	public User(String username, String password){
 		this.username = username;
 		this.password = password;
 	}
 	
-	public boolean equals(String username, String password) {
-		
-		if(this.username.equals(username)&&this.password.equals(password)) {
+	/**
+	 * Compares two User and returns true if username and password are identical.
+	 * 
+	 * @param user_1
+	 * @param user_2
+	 * @return
+	 */
+	public boolean equals(User user_1, User user_2) {
+		if(user_1.username.equals(user_2.username)&&(user_1.password.equals(user_2.password))) {
 			return true;
-		} else {
+		}
+		else {
 			return false;
 		}
 	}
