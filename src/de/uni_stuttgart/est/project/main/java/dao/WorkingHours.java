d83263d21702c953ea3b191d10f46a38fd8115b3
PhilippTeichert <34233007+PhilippTeichert@users.noreply.github.com>
2019-04-28 12:49:42 +0200

    Create WorkingHours.java

---
+++ src/de/uni_stuttgart/est/project/main/java/dao/WorkingHours.java
@@ -0,0 +1,44 @@
+package dao;
+
+public class WorkingHours extends OrderComponent{
+
+	private OrderComponent orderComponent;
+	private String description;
+	private int hours;
+	private double ratePerHour;
+	private String outcome;
+	
+	public WorkingHours(OrderComponent orderComponent, String description, int hours, double ratePerHour) {
+		this.orderComponent = orderComponent;
+		this.description = description;
+		this.hours = hours;
+		this.ratePerHour = ratePerHour;
+	}
+		
+	private double adding(double x, double y) {
+		return x*y;
+	}
+	
+	public String toString() {
+		return outcome
+				.concat(this.description.toString()+": ")
+				.concat(String.valueOf(hours)+" Stunde(n), ")
+				.concat(String.valueOf(ratePerHour)+" Euro/Stunde, ")
+				.concat("Gesamt: "+String.valueOf(adding(hours,ratePerHour))+" Euro");
+	}
+		
+
+	@Override
+	public double price() {
+		double sum = super.price();
+		return adding(sum,adding(hours,ratePerHour));
+	}
+	
+	@Override
+	public String summary() {
+		String summary = super.summary();
+		String ownSummary = toString();
+		return summary.concat(System.lineSeparator()+ownSummary);
+	}
+	
+}
